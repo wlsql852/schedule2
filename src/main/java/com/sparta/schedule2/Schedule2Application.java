484450c1f24c683b0246c81355ea0c1bf13f3633
wlsql852 <wlsql852@naver.com>
2024-08-30 22:38:25 +0900

    Pagenation을 했을때 정리된 ResponseEntity가 나오도록 @EnableSpringDataWebSupport추가

--- src/main/java/com/sparta/schedule2/Schedule2Application.java
+++ src/main/java/com/sparta/schedule2/Schedule2Application.java
@@ -3,9 +3,11 @@ package com.sparta.schedule2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Schedule2Application {
 
     public static void main(String[] args) {
